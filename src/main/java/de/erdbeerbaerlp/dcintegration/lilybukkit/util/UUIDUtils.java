31abe409bc606a14113e218dde40e10fc0b86390
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2022-09-08 10:29:13 +0200

    Update to Lilybukkit a2.0.0.0

--- src/main/java/de/erdbeerbaerlp/dcintegration/lilybukkit/util/UUIDUtils.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/lilybukkit/util/UUIDUtils.java
@@ -1,17 +1,17 @@
 package de.erdbeerbaerlp.dcintegration.lilybukkit.util;
 
-import org.apache.commons.io.Charsets;
 import org.bukkit.entity.Player;
 
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class UUIDUtils {
 
     public static UUID getUUIDFromName(String name) {
-        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8));
+        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(StandardCharsets.UTF_8));
     }
     public static UUID getUUIDFromPlayer(Player p) {
-        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + p.getName()).getBytes(Charsets.UTF_8));
+        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + p.getName()).getBytes(StandardCharsets.UTF_8));
     }
 
 }
